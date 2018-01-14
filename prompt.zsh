478cf9c5b288d3e4624c004805e4e4b5ab62023c
Jonathan Koren <jdkoren@gmail.com>
2018-01-14 15:10:29 -0800

    Add HG prompts and convert tabs to spaces

--- prompt.zsh
+++ prompt.zsh
@@ -5,52 +5,52 @@ git_remote_ahead_behind() {
         ahead=$(command git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
         behind=$(command git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
 
-		git_state=""
-		if [ ${ahead} -gt 0 ]
-		then
-			git_state="${git_state}  %{$FG[010]%}${ahead##*( )}↑"
-		fi
+        git_state=""
+        if [ ${ahead} -gt 0 ]
+        then
+            git_state="${git_state}  %{$FG[010]%}${ahead##*( )}↑"
+        fi
 
-		if [ ${behind} -gt 0 ]
-		then
-			git_state="${git_state}  %{$FG[009]%}${behind##*( )}↓"
-		fi
+        if [ ${behind} -gt 0 ]
+        then
+            git_state="${git_state}  %{$FG[009]%}${behind##*( )}↓"
+        fi
 
-		echo "${git_state}"
+        echo "${git_state}"
     fi
 }
 
 # If a merge, rebase, or bisect is in progress, returns $ZSH_THEME_GIT_PROMPT_MERGING,
 # $ZSH_THEME_GIT_PROMPT_REBASING, or $ZSH_THEME_GIT_PROMPT_BISECTING respectively.
 git_merge_state() {
-	git_dir=$(command git rev-parse --git-dir 2>/dev/null)
-	if [[ -e "${git_dir}/MERGE_HEAD" ]]
-	then
-		echo "$ZSH_THEME_GIT_PROMPT_MERGING"
-	elif [[ -e "${git_dir}/rebase-merge" || -e "${git_dir}/rebase-apply" ]]
-	then
-		echo "$ZSH_THEME_GIT_PROMPT_REBASING"
-	elif [[ -e "${git_dir}/CHERRY_PICK_HEAD" ]]
-	then
-		echo "$ZSH_THEME_GIT_PROMPT_CHERRYPICKING"
-	elif [[ -e "${git_dir}/BISECT_LOG" ]]
-	then
-		echo "$ZSH_THEME_GIT_PROMPT_BISECTING"
-	fi
+    git_dir=$(command git rev-parse --git-dir 2>/dev/null)
+    if [[ -e "${git_dir}/MERGE_HEAD" ]]
+    then
+        echo "$ZSH_THEME_GIT_PROMPT_MERGING"
+    elif [[ -e "${git_dir}/rebase-merge" || -e "${git_dir}/rebase-apply" ]]
+    then
+        echo "$ZSH_THEME_GIT_PROMPT_REBASING"
+    elif [[ -e "${git_dir}/CHERRY_PICK_HEAD" ]]
+    then
+        echo "$ZSH_THEME_GIT_PROMPT_CHERRYPICKING"
+    elif [[ -e "${git_dir}/BISECT_LOG" ]]
+    then
+        echo "$ZSH_THEME_GIT_PROMPT_BISECTING"
+    fi
 }
 
 function parse_git_state() {
-	ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-	ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
 
-	git_info="%{$FG[011]%}${ref#refs/heads/}"
-	merge_state=$(git_merge_state)
-	if [[ -n ${merge_state} ]]
-	then
-		git_info="${git_info}${merge_state}"
-	else
-		git_info="${git_info}$(parse_git_dirty)$(git_remote_ahead_behind)"
-	fi
+    git_info="%{$FG[011]%}${ref#refs/heads/}"
+    merge_state=$(git_merge_state)
+    if [[ -n ${merge_state} ]]
+    then
+        git_info="${git_info}${merge_state}"
+    else
+        git_info="${git_info}$(parse_git_dirty)$(git_remote_ahead_behind)"
+    fi
 
-	echo "$ZSH_THEME_GIT_PROMPT_PREFIX${git_info}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${git_info}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
--- themes/jdk.zsh-theme
+++ themes/jdk.zsh-theme
@@ -1,3 +1,4 @@
+# For git plugin
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$FG[003]%}±"
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
 ZSH_THEME_GIT_PROMPT_DIRTY="*"
@@ -8,6 +9,12 @@ ZSH_THEME_GIT_PROMPT_REBASING="%{$FG[219]%} R"
 ZSH_THEME_GIT_PROMPT_BISECTING="%{$FG[219]%} B"
 ZSH_THEME_GIT_PROMPT_CHERRYPICKING="%{$FG[219]%} C"
 
+# For mercurial plugin
+ZSH_THEME_HG_PROMPT_PREFIX="%{$FG[003]%}☿"
+ZSH_THEME_HG_PROMPT_SUFFIX=""
+ZSH_THEME_HG_PROMPT_DIRTY="*"
+ZSH_THEME_HG_PROMPT_CLEAN=""
+
 ZSH_SPECTRUM_TEXT="To iterate is human; to recurse, divine"
 
 # See http://geoff.greer.fm/lscolors/
@@ -16,4 +23,4 @@ export LS_COLORS="di=36;40:ln=0;46:so=35;40:pi=35;40:ex=32;40:bd=31;40:cd=31;40:
 
 local status_indicator="%(?|%{$FG[010]%}%# |%{$FG[009]%}%# )"
 PROMPT="%B%{$FG[045]%}%n %{$FG[011]%}%~$status_indicator%{$reset_color%}%B"
-RPROMPT='%{$FG[011]%}$(parse_git_state)%{$reset_color%}'
+RPROMPT='%{$FG[011]%}$(parse_git_state)$(hg_prompt_info)%{$reset_color%}'
